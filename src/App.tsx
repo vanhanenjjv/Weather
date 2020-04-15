aaae1189410929c938c5206dc0a93cd05f48ad8c
Janne Vanhanen <janne.j.v.vanhanen@icloud.com>
2020-04-16 02:41:14 +0300

    Format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+
 const App: React.FC = () => (
-  <div>
+  <div className="foo">
     Hello world!
   </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
